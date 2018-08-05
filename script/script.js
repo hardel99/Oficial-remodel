109eccf71ccb45c1487fa8513fdd319f6451bcc7
hardel99 <harold-delgado1999@hotmail.com>
2018-08-04 18:16:57 -0600

    Wacha ese efecto men

--- script/script.js
+++ script/script.js
@@ -13,47 +13,59 @@ $(document).ready(function() {
       "margin-top": height - 190 + "px"
     });
 
-    if(width>1000){
+    if (width > 1000) {
       $('section#seccion').css({
-        "height": height - 120 + "px"         //para evitar cambios en tamaños de pantalla y que sea responsive
+        "height": height - 120 + "px" //para evitar cambios en tamaños de pantalla y que sea responsive
       });
-    }else{
-      if(width<700){
+
+      $(".squer").css({
+        "height": height - 200 + "px"
+      });
+    } else {
+      if (width < 700) {
         $('section#seccion').css({
-          "height": (height * 2.8) - 120 + "px"         //para evitar cambios en tamaños de pantalla y que sea responsive
+          "height": (height * 2.8) - 120 + "px" //para evitar cambios en tamaños de pantalla y que sea responsive
+        });
+
+        $(".squer").css({
+          "height": height - 70 + "px"
         });
 
         $(".cir").css({
-          "height": height/1.3 +"px"
+          "height": height / 1.3 + "px"
         });
 
         $('.mein-cont section').css({
-          "height" : height + "px"
+          "height": height + "px"
         });
 
         $('.serfv').css({
-          "height" : height - 50 + "px"
+          "height": height - 50 + "px"
         });
-      }else{
+      } else {
         $('section#seccion').css({
-          "height": height - 120 + "px"         //para evitar cambios en tamaños de pantalla y que sea responsive
+          "height": height - 120 + "px" //para evitar cambios en tamaños de pantalla y que sea responsive
+        });
+
+        $(".squer").css({
+          "height": height - 120 + "px"
         });
       }
 
-      $('.logo').attr('src','pictures/logo.png');
+      $('.logo').attr('src', 'pictures/logo.png');
     }
   }
 
 
   function menuMovil() {
-    $(".haided").click(function () {
+    $(".haided").click(function() {
 
-      if(displayed == 0){
+      if (displayed == 0) {
         $("#menu").animate({
           left: "0"
         });
         displayed = 1;
-      }else{
+      } else {
         $("#menu").animate({
           left: "-100%"
         });
@@ -61,7 +73,6 @@ $(document).ready(function() {
       }
     })
   }
-
 
   $(document).scroll(function() {
     var scrollTop = $(this).scrollTop();
@@ -76,18 +87,86 @@ $(document).ready(function() {
 
   });
 
-	//Desde aqui empiezo a hacer mi magia
+  //Desde aqui empiezo a hacer mi magia
 
-	var sigsec = $('#seccion');
+  //Watch this
+  var triger = document.getElementById("forTher");
+  var imag = document.getElementById("salM");
 
-	sigsec.waypoint(function (qCam) {
-    if(width>1000){
-      if(qCam == "down"){
-  			$('.logo').attr('src','pictures/logo_dark.png');
-  		}else{
-  			$('.logo').attr('src','pictures/logo.png');
-  		}
+  var maus = {
+    pseudX: 0,
+    pseudY: 0,
+    x: 0,
+    y: 0,
+    updatePosition: function(event) {
+      var e = event || window.event;
+      this.x = e.clientX - this.pseudX;
+      this.y = (e.clientY - this.pseudY) * -1;
+    },
+    setOrigin: function(e) {
+      this.pseudX = e.offsetLeft + Math.floor(e.offsetWidth / 2);
+      this.pseudY = e.offsetTop + Math.floor(e.offsetHeight / 2);
+    },
+    show: function() {
+      return '(' + this.x + ', ' + this.y + ')';
     }
-	}, { offset: '5%'});
-	//listo prro
+  }
+
+  maus.setOrigin(triger);
+
+  var count = 0;
+  var framsps = 10;
+  var actualiza = function() {
+    return count++ % framsps === 0;
+  };
+
+  var onMouseEnterHandler = function(event) {
+    update(event);
+  };
+
+  var onMouseLeaveHandler = function() {
+    imag.style = "";
+  };
+
+  var onMouseMoveHandler = function(event) {
+    if (actualiza()) {
+      update(event);
+    }
+  };
+
+  var update = function(event) {
+    maus.updatePosition(event);
+    transformerYa(
+      (maus.y / imag.offsetHeight / 2).toFixed(2),
+      (maus.x / imag.offsetWidth / 2).toFixed(2)
+    );
+  };
+
+  var transformerYa = function(x, y) {
+    var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
+    imag.style.transform = style;
+    imag.style.webkitTransform = style;
+    imag.style.mozTransform = style;
+    imag.style.msTransform = style;
+    imag.style.oTransform = style;
+  };
+
+  triger.onmouseenter = onMouseEnterHandler;
+  triger.onmouseleave = onMouseLeaveHandler;
+  triger.onmousemove = onMouseMoveHandler;
+
+  var sigsec = $('#seccion');
+
+  sigsec.waypoint(function(qCam) {
+    if (width > 1000) {
+      if (qCam == "down") {
+        $('.logo').attr('src', 'pictures/logo_dark.png');
+      } else {
+        $('.logo').attr('src', 'pictures/logo.png');
+      }
+    }
+  }, {
+    offset: '5%'
+  });
+  //listo prro
 });
